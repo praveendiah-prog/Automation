118d45d71f8303e689ace1ef1ac8109771e1e04a
praveendiah-prog <praveendiah@gmail.com>
2026-06-05 14:33:20 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,6 +7,7 @@ print(is_even(10))  # True
 print(is_even(7))   # False
 num = int(input("Enter a number: "))
 if num % 2 == 0:
+    eiiiii
     print("Even Number")
 else:
     great
@@ -15,4 +16,5 @@ else:
     print("Odd Number")
     enna
     so what
+    konruva
     i am the linf
